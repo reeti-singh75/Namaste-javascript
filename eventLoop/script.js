a606e94d63820e39b492880eb4782ebbfe431ec3
Reeti Singh <reetisingh24@navgurukul.org>
2026-05-18 22:07:45 +0530

    Event loop is added

---
+++ eventLoop/script.js
@@ -0,0 +1,49 @@
+
+
+
+// function a(){
+//     console.log("a"); 
+// }
+
+// a();
+// console.log("End");
+
+
+
+// ==========================================
+
+
+// console.log("Start");
+
+// setTimeout(function cd(){
+
+//     console.log("Callback");
+    
+// },5000);
+// console.log("End");
+
+
+
+
+// =============================================
+
+
+
+
+console.log("Start");
+
+document.getElementById("btn")
+.addEventListener("click", function cb(){
+ console.log("callback");
+ 
+})
+
+console.log("End");
+
+
+
+
+
+
+
+  
