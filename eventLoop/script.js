01f78a43648ff36a513afcb53a94111779a0b57c
Reeti Singh <reetisingh24@navgurukul.org>
2026-05-19 11:42:44 +0530

    added fach example

--- eventLoop/script.js
+++ eventLoop/script.js
@@ -30,19 +30,31 @@
 
 
 
+// console.log("Start");
+
+// document.getElementById("btn")
+// .addEventListener("click", function cb(){
+//  console.log("callback");
+ 
+// })
+
+// console.log("End");
+
+
+//=====================================================
+
+
+
 console.log("Start");
 
-document.getElementById("btn")
-.addEventListener("click", function cb(){
- console.log("callback");
- 
-})
+// fetch() makes a network request
+// The callback function (the .then block) goes into the MicroTask Queue
+fetch("https://api.example.com/data")
+.then(function cb() {
+console.log("Callback from fetch");
+});
 
 console.log("End");
-
-
-
-
 
 
 
